c7df47fa9cc39c72ad620b4e91f920714789892d
DenisGizatulin <denis_gizatulin@outlook.com>
2024-12-13 12:54:16 +0300

    PEP8 formatted

--- domains_checker.py
+++ domains_checker.py
@@ -1,8 +1,9 @@
 import re
 
 
-VALID_DOMAIN_REGEX = r'^(?!-)([a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z]{2,}$' # Регулярное выражение для поиска доменов в тексте
-FIND_DOMAIN_REGEX = r'(?<!-)\"?([a-zA-Z0-9-]{1,63}\.[a-zA-Z]{2,})\"?' # Регулярное выражение для поиска корректных доменов в тексте
+VALID_DOMAIN_REGEX = r"^(?!-)([a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z]{2,}$"  # Регулярное выражение для поиска доменов в тексте
+FIND_DOMAIN_REGEX = r"(?<!-)\"?([a-zA-Z0-9-]{1,63}\.[a-zA-Z]{2,})\"?"  # Регулярное выражение для поиска корректных доменов в тексте
+
 
 def is_valid_domain(domain):
     return re.match(VALID_DOMAIN_REGEX, domain) is not None
@@ -10,14 +11,16 @@ def is_valid_domain(domain):
 
 def check_domains_in_file(file_path):
     # Открываем файл и читаем его содержимое
-    with open(file_path, 'r', encoding='utf-8') as file:
+    with open(file_path, "r", encoding="utf-8") as file:
         text = file.read()
 
     # Находим все возможные совпадения по регулярному выражению
     raw_domains = re.findall(FIND_DOMAIN_REGEX, text)
 
     # Фильтруем и оставляем только корректные доменные имена
-    valid_domains = [domain for domain in raw_domains if is_valid_domain(domain)]
+    valid_domains = [
+        domain for domain in raw_domains if is_valid_domain(domain)
+    ]
 
     return valid_domains
 
@@ -25,7 +28,7 @@ def check_domains_in_file(file_path):
 def find_domains_in_text(file_path):
 
     # Открываем файл и читаем его содержимое
-    with open(file_path, 'r', encoding='utf-8') as file:
+    with open(file_path, "r", encoding="utf-8") as file:
         text = file.read()
 
     # Находим все совпадения по регулярному выражению
--- domains_checker_TEST.py
+++ domains_checker_TEST.py
@@ -3,13 +3,14 @@
 import unittest
 from domains_checker import is_valid_domain
 
-class TestDomainValidation(unittest.TestCase): #
+
+class TestDomainValidation(unittest.TestCase):  #
     def test_valid_domains(self):
         valid_domains = [
             "example.com",
             "sub-domain.example.org",
             "another-example.co.uk",
-            "example123.biz"
+            "example123.biz",
         ]
         for domain in valid_domains:
             self.assertTrue(is_valid_domain(domain))
@@ -22,7 +23,7 @@ class TestDomainValidation(unittest.TestCase): #
             "example_123.com",
             "123.456.789.012",
             "example.c",
-            "example@domain.com"
+            "example@domain.com",
         ]
         for domain in invalid_domains:
             self.assertFalse(is_valid_domain(domain))
